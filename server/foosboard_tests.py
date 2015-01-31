5b37cc50077d447665f0a3674dac6864c3c274fe
Eno Compton 4 <eno4@ecom.com>
2015-01-30 23:42:13 -0700

    Add coverage for creating a game

--- server/foosboard_tests.py
+++ server/foosboard_tests.py
@@ -1,8 +1,7 @@
 import unittest
 import json
-from foosboard import app
-from flask_sqlalchemy import SQLAlchemy
-from config import TestConfig
+from foosboard import app, db
+from foosboard.models import Game
 
 
 class FoosboardTestCase(unittest.TestCase):
@@ -10,13 +9,38 @@ class FoosboardTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app
         self.client = app.test_client()
-        self.db = SQLAlchemy(self.app)
+        db.drop_all()
+        db.create_all()
 
-    def test_empty_db(self):
+    def tearDown(self):
+        db.drop_all()
+
+    def test_endpoint_returns_no_games_by_default(self):
         rv = self.client.get('/api/games',
                              headers={'Accept': 'application/json'})
 
         expected_response = {'games': []}
+
+        self.assertEqual(json.loads(rv.data), expected_response)
+
+    def test_endpoint_returns_all_persisted_games(self):
+        game = Game("EC", "BB", "TG", "NB")
+        game.team1score = 3
+        game.team2score = 5
+
+        db.session.add(game)
+        db.session.commit()
+
+        rv = self.client.get('/api/games',
+                             headers={'Accept': 'application/json'})
+
+        expected_response = {'games': [{
+            'id': 1,
+            'team_1_score': 3,
+            'team_2_score': 5,
+            'team_1': {'offense': 'BB', 'defense': 'EC'},
+            'team_2': {'offense': 'TG', 'defense': 'NB'}
+        }]}
 
         self.assertEqual(json.loads(rv.data), expected_response)
 
