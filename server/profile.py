fd4eba67386b3fbbb7b3cfa7d270d2363bac06b2
tygern <tygern@gmail.com>
2015-02-27 19:42:19 -0700

    Add profiler

---
+++ server/profile.py
@@ -0,0 +1,9 @@
+import os
+from foosboard import app
+
+from werkzeug.contrib.profiler import ProfilerMiddleware
+from app import app
+
+app.config['PROFILE'] = True
+app.wsgi_app = ProfilerMiddleware(app.wsgi_app, restrictions=[30])
+app.run(debug = True)
