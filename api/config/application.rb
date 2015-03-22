4fc2c52a46d7b691e7806c1cfe20aa6d8bc46661
Eno Compton 4 <eno4@ecom.com>
2015-03-22 16:40:02 -0600

    Add CORS to Rails api

--- api/config/application.rb
+++ api/config/application.rb
@@ -10,10 +10,19 @@ Bundler.setup(*Rails.groups)
 
 # Enable Pry as default for `rails c`
 require "pry-rails"
+require "rack/cors"
 
 module Api
   class Application < Rails::Application
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins("*")
+
+        resource("*", headers: :any, methods: [:get, :post, :delete, :patch, :options])
+      end
+    end
   end
 end
